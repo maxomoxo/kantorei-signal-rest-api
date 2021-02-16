4d76377f3e95ec5fe53d5ce07c2f9bf24c3730aa
maxomoxo <marco.haider02@gmail.com>
2021-02-16 08:28:38 +0100

    signal rest push

---
+++ Dockerfile
@@ -0,0 +1,53 @@
+FROM golang:1.13-buster AS buildcontainer
+
+ARG SIGNAL_CLI_VERSION=0.6.11
+ARG SWAG_VERSION=1.6.7
+
+ENV GIN_MODE=release
+
+RUN apt-get update \
+	&& apt-get install -y --no-install-recommends wget default-jre software-properties-common git locales \
+	&& rm -rf /var/lib/apt/lists/* 
+
+RUN sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && \
+    dpkg-reconfigure --frontend=noninteractive locales && \
+    update-locale LANG=en_US.UTF-8
+
+ENV LANG en_US.UTF-8
+
+RUN cd /tmp/ \
+	&& git clone https://github.com/swaggo/swag.git swag-${SWAG_VERSION} \	
+	&& cd swag-${SWAG_VERSION} \
+	&& git checkout v${SWAG_VERSION} \
+	&& make \
+	&& cp /tmp/swag-${SWAG_VERSION}/swag /usr/bin/swag \
+	&& rm -r /tmp/swag-${SWAG_VERSION}
+
+RUN cd /tmp/ \
+	&& git clone https://github.com/AsamK/signal-cli.git signal-cli-${SIGNAL_CLI_VERSION} \
+	&& cd signal-cli-${SIGNAL_CLI_VERSION} \
+	&& git checkout v${SIGNAL_CLI_VERSION} \
+	&& ./gradlew build \
+	&& ./gradlew installDist \
+	&& ln -s /tmp/signal-cli-${SIGNAL_CLI_VERSION}/build/install/signal-cli/ /tmp/signal-cli
+
+COPY src/api /tmp/signal-cli-rest-api-src/api
+COPY src/main.go /tmp/signal-cli-rest-api-src/
+COPY src/go.mod /tmp/signal-cli-rest-api-src/
+COPY src/go.sum /tmp/signal-cli-rest-api-src/
+
+RUN cd /tmp/signal-cli-rest-api-src && swag init && go build
+
+# Start a fresh container for release container
+FROM adoptopenjdk:11-jre-hotspot
+
+COPY --from=buildcontainer /tmp/signal-cli-rest-api-src/signal-cli-rest-api /usr/bin/signal-cli-rest-api
+COPY --from=buildcontainer /tmp/signal-cli /opt/signal-cli
+
+RUN ln -s /opt/signal-cli/bin/signal-cli /usr/bin/signal-cli
+RUN mkdir -p /signal-cli-config/
+RUN mkdir -p /home/.local/share/signal-cli
+
+EXPOSE 8080
+
+ENTRYPOINT ["signal-cli-rest-api"]
---
+++ src/main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"flag"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/bbernhard/signal-cli-rest-api/api"
+	_ "github.com/bbernhard/signal-cli-rest-api/docs"
+
+)
+
+
+
+// @title Signal Cli REST API
+// @version 1.0
+// @description This is the Signal Cli REST API documentation.
+
+// @tag.name General
+// @tag.description List general information.
+
+// @tag.name Devices
+// @tag.description Register and link Devices.
+
+// @tag.name Groups
+// @tag.description Create, List and Delete Signal Groups.
+
+// @tag.name Messages
+// @tag.description Send and Receive Signal Messages.
+
+// @host 127.0.0.1:8080
+// @BasePath 
+func main() {
+    
+	
+	signalCliConfig := flag.String("signal-cli-config", "/home/.local/share/signal-cli/", "Config directory where signal-cli config is stored")
+	attachmentTmpDir := flag.String("attachment-tmp-dir", "/tmp/", "Attachment tmp directory")
+	flag.Parse()
+
+	router := gin.Default()
+	
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "HEAD", "OPTIONS", "POST", "PUT"},
+		AllowHeaders:     []string{"Access-Control-Allow-Headers", "Origin,Accept", "X-Requested-With", "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers", "authorization"},
+		AllowCredentials: true,	
+	}))
+	
+	log.Info("Started Signal Messenger REST API")
+
+	api := api.NewApi(*signalCliConfig, *attachmentTmpDir)	
+	v1 := router.Group("/v1")
+	{
+		about := v1.Group("/about")
+		{
+			about.GET("", api.About)
+		}
+
+		register := v1.Group("/register")
+		{
+			register.POST(":number", api.RegisterNumber)
+			register.POST(":number/verify/:token", api.VerifyRegisteredNumber)
+		}
+
+		sendV1 := v1.Group("/send")
+		{
+			sendV1.POST("", api.Send)
+		}
+
+		receive := v1.Group("/receive")
+		{
+			receive.GET(":number", api.Receive)
+		}
+
+		groups := v1.Group("/groups")
+		{
+			groups.POST(":number", api.CreateGroup)
+			groups.GET(":number", api.GetGroups)
+			groups.DELETE(":number/:groupid", api.DeleteGroup)
+		}
+
+		link := v1.Group("qrcodelink")
+		{
+			link.GET("", api.GetQrCodeLink)
+		}
+	}
+
+	v2 := router.Group("/v2")
+	{
+		sendV2 := v2.Group("/send")
+		{
+			sendV2.POST("", api.SendV2)
+		}
+	}
+
+	swaggerUrl := ginSwagger.URL("http://127.0.0.1:8080/swagger/doc.json")
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, swaggerUrl))
+	
+	
+	router.Run()
+
+
+}
